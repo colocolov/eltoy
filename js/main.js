8d5bb5ed55870a216517110d4f109764af157c9f
Colocolov Stas <colocolov.stas@gmail.com>
2021-02-05 13:00:28 +0200

    cr: фильтр работ

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+// popup окна
 $(document).ready(function () {
   $(".works__photo-link").magnificPopup({ type: "image" });
 });
+//-----
+
+// фильтр трендов
+let articles = document.querySelectorAll(".works__photo");
+// console.log(articles);
+function doAction(value) {
+  let types = document.querySelectorAll(".works__type");
+  for (let type of types) {
+    if (value == type.value) {
+      type.classList.add("works__type_active");
+    } else {
+      type.classList.remove("works__type_active");
+    }
+  }
+  for (let article of articles) {
+    if (value !== "all") {
+      // console.log(value);
+      // console.log(article.dataset.category);
+      //цикл if
+      if (article.dataset.category !== value) {
+        article.classList.add("works__hide");
+      } else {
+        article.classList.remove("works__hide");
+      } //цикл if
+    } else {
+      article.classList.remove("works__hide");
+    }
+  }
+}
+//-----
